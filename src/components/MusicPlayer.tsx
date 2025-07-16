d1ae48165d223eac0271701874dbf869655c9e35
Erikles Geyzon <geyzon100@gmail.com>
2025-07-16 00:33:48 -0300

    Remove unused wedding song and update audio source path in MusicPlayer component

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -8,7 +8,7 @@ interface MusicPlayerProps {
 }
 
 const MusicPlayer = ({
-  audioSrc = "src/assets/Stephen_Sanchez-Until_I_Found_You.mp3",
+  audioSrc = "public/assets/Stephen_Sanchez-Until_I_Found_You.mp3",
   title = "Música do Casamento",
 }: MusicPlayerProps) => {
   const [isPlaying, setIsPlaying] = useState(true);
